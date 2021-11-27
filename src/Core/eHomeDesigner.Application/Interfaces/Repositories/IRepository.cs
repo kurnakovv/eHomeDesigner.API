a91633f966d228b3789af5bc994721e7e1571e86
KurnakovMaksim <maksk210@gmail.com>
2021-11-27 15:05:03 +0900

    Add IRepository

---
+++ src/Core/eHomeDesigner.Application/Interfaces/Repositories/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using eHomeDesigner.Application.Interfaces.POCOs;
+using System;
+using System.Collections.Generic;
+
+namespace eHomeDesigner.Application.Interfaces.Repositories
+{
+    public interface IRepository
+    {
+        IReadOnlyCollection<IPOCO> GetAll();
+        IPOCO GetById(Guid id);
+        void Add(IPOCO poco);
+        void Update(Guid id, IPOCO poco);
+        void DeleteById(Guid id);
+    }
+}
