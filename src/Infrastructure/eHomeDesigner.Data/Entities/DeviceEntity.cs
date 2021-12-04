0272d336c63a54781f10d7043997867b8c3398eb
KurnakovMaksim <maksk210@gmail.com>
2021-12-04 17:10:07 +0900

    Add DeviceEntity

---
+++ src/Infrastructure/eHomeDesigner.Data/Entities/DeviceEntity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eHomeDesigner.Data.Entities;
+
+public class DeviceEntity : BaseEntity
+{
+    public int Price { get; set; }
+    public int SquareMeters { get; set; }
+    public int EnergyPerHour { get; set; }
+    public string Author { get; set; }
+    public string DeviceType { get; set; }
+}
