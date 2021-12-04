25d3d85574ea115e5e9a9c7443669841a9bd61ae
KurnakovMaksim <maksk210@gmail.com>
2021-12-04 18:04:25 +0900

    Add private set for BaseEntity
    For EntityFrameworkCore

--- src/Infrastructure/eHomeDesigner.Data/Entities/BaseEntity.cs
+++ src/Infrastructure/eHomeDesigner.Data/Entities/BaseEntity.cs
@@ -8,6 +8,6 @@ namespace eHomeDesigner.Data.Entities;
 
 public class BaseEntity : IEntity
 {
-    public Guid Id { get; } = Guid.NewGuid();
-    public DateTime DateOfCreate { get; } = DateTime.Now;
+    public Guid Id { get; private set; } = Guid.NewGuid();
+    public DateTime DateOfCreate { get; private set; } = DateTime.Now;
 }
