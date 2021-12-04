40ecd445998ba171790d270bbcc2cf72d11541eb
KurnakovMaksim <maksk210@gmail.com>
2021-12-04 17:20:24 +0900

    Add DeviceDbContext

---
+++ src/Infrastructure/eHomeDesigner.Data/Contexts/DeviceDbContext.cs
@@ -0,0 +1,16 @@
+﻿using eHomeDesigner.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eHomeDesigner.Data.Contexts;
+
+public class DeviceDbContext : DbContext
+{
+    public DeviceDbContext(DbContextOptions<DeviceDbContext> options) : base(options) { }
+
+    public DbSet<DeviceEntity> Devices { get; set; }
+}
