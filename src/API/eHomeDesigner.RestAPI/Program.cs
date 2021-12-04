e5cd4246a0195f2139f055995ee966be9fa9e4d1
KurnakovMaksim <maksk210@gmail.com>
2021-12-04 14:58:10 +0900

    Add usings in Program

--- src/API/eHomeDesigner.RestAPI/Program.cs
+++ src/API/eHomeDesigner.RestAPI/Program.cs
@@ -1,5 +1,13 @@
 using Microsoft.OpenApi.Models;
+using System.Net.Http.Json;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 var builder = WebApplication.CreateBuilder(args);
 
