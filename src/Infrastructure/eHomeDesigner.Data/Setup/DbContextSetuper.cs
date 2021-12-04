ee4a0278d11ebdd5b0f29c0083573bc4b3939e91
KurnakovMaksim <maksk210@gmail.com>
2021-12-04 17:31:46 +0900

    Add DeviceDbContext in DbContextSetuper

---
+++ src/Infrastructure/eHomeDesigner.Data/Setup/DbContextSetuper.cs
@@ -0,0 +1,21 @@
+﻿using eHomeDesigner.Data.Contexts;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eHomeDesigner.Data.Setup;
+
+public static class DbContextSetuper
+{
+    public static void SetupDbContext(this IServiceCollection services, string connectionString)
+    {
+        services.AddDbContext<DeviceDbContext>(options =>
+        {
+            options.UseSqlServer(connectionString);
+        });
+    }
+}
