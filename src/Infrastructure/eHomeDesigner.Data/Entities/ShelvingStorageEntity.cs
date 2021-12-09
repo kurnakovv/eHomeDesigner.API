e1cdaf5e87ce515c66074f6aa00ed242062035c2
KurnakovMaksim <maksk210@gmail.com>
2021-12-09 11:48:42 +0900

    Add ShelvingStorageEntity

---
+++ src/Infrastructure/eHomeDesigner.Data/Entities/ShelvingStorageEntity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eHomeDesigner.Data.Entities;
+
+public class ShelvingStorageEntity : BaseEntity
+{
+    public int Price { get; set; }
+    public int SquareMeters { get; set; }
+    public int Length { get; set; }
+}
