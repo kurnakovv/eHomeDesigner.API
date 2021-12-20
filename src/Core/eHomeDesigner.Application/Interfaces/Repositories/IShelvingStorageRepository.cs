28e5003f925cb74989634c7d6bb893580c7cb3d6
KurnakovMaksim <maksk210@gmail.com>
2021-12-20 11:23:29 +0900

    Add ShelvingStorageRepository

---
+++ src/Core/eHomeDesigner.Application/Interfaces/Repositories/IShelvingStorageRepository.cs
@@ -0,0 +1,12 @@
+﻿using eHomeDesigner.Application.Interfaces.POCOs.Furnitures;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eHomeDesigner.Application.Interfaces.Repositories;
+
+public interface IShelvingStorageRepository<TShelvingStorage> : IRepository<TShelvingStorage> where TShelvingStorage : BaseShelvingStorage
+{
+}
